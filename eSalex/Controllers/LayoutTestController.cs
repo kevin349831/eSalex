eece84fe9ee541a82a4be54e669f591785b1f475
kevin349831 <kevin349831@gmail.com>
2017-03-24 15:48:44 +0800

    video 4 end

---
+++ eSalex/Controllers/LayoutTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSalex.Controllers
+{
+    public class LayoutTestController : Controller
+    {
+        /// <summary>
+        /// 測試主版頁面
+        /// </summary>
+        /// <returns></returns>
+        // GET: LayoutTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
